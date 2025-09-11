6007878ac6c3e2d0ec1a3c34af28e2fb72a803a5
Averil Dwi Yokta Mauladani <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2025-09-11 15:13:20 +0700

    Add files via upload

---
+++ Latihan KKA.py
@@ -0,0 +1,35 @@
+import math
+
+print("Soal no 1")
+integer = 1
+float = 1.1
+string = "Averil"
+listt = [1,2,3]
+print(type(integer))
+print(type(float))
+print(type(string))
+print(type(listt))
+
+print("\nSoal no 2")
+listbelanja = ["beras", "minyak", "telur"]
+listbelanja.extend(["gula", "kopi"])
+print('Daftar Belanja')
+for i in range(5):
+    print(listbelanja[i])
+    
+print("\nSoal no 3")
+belanjaan = {
+    "beras" : 12000,
+    "minyak" : 17000,
+    "telur" : 24000,
+    "gula" : 15000,
+    "kopi" : 20000
+}
+total = sum(belanjaan.values())
+print("Total belanja =",total)
+
+print("\nSoal no 4")
+radius = int(input("Jari-jari = "))
+area = math.pi * radius * radius
+circumference = 2*math.pi*radius
+print("Luas =",area,"\nKeliling =",circumference)
